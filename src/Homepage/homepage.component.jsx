f9f9e74262e6449077c23fa2f00f2892e925d0e6
Zakeria Ali <zak.fined@gmail.com>
2019-11-24 19:56:16 +0000

    Add base styling for the homepage

---
+++ src/Homepage/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Homepage = () => (
+	<div className="homepage">
+		<div className="product-wrapper">
+			<div className="product-wrapper--item">
+				<div className="product-wrapper--item__content">
+					<h1>Hats</h1>
+					<p><a href="#">Shop now</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+)
+
+export default Homepage
