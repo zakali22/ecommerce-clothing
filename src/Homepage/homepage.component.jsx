0f7c084001e765a2bc299578866e509f48acb294
Zakeria Ali <zak.fined@gmail.com>
2019-11-26 04:27:56 +0000

    Add files for homepage component

--- src/Homepage/homepage.component.jsx
+++ src/Homepage/homepage.component.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import './homepage.styles.scss'
 
 const Homepage = () => (
 	<div className="homepage">
@@ -6,6 +7,30 @@ const Homepage = () => (
 			<div className="product-wrapper--item">
 				<div className="product-wrapper--item__content">
 					<h1>Hats</h1>
+					<p><a href="#">Shop now</a></p>
+				</div>
+			</div>
+			<div className="product-wrapper--item">
+				<div className="product-wrapper--item__content">
+					<h1>Jackets</h1>
+					<p><a href="#">Shop now</a></p>
+				</div>
+			</div>
+			<div className="product-wrapper--item">
+				<div className="product-wrapper--item__content">
+					<h1>Trousers</h1>
+					<p><a href="#">Shop now</a></p>
+				</div>
+			</div>
+			<div className="product-wrapper--item">
+				<div className="product-wrapper--item__content">
+					<h1>Mens</h1>
+					<p><a href="#">Shop now</a></p>
+				</div>
+			</div>
+			<div className="product-wrapper--item">
+				<div className="product-wrapper--item__content">
+					<h1>Womens</h1>
 					<p><a href="#">Shop now</a></p>
 				</div>
 			</div>
