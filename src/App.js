be2bdd40f51bad3a3c885a67fcf0887ba4d421a0
Zakeria Ali <zak.fined@gmail.com>
2019-11-29 21:39:27 +0000

    Adding and styling header component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import {Switch, Route} from "react-router-dom"
 
 import Homepage from "./pages/Homepage/homepage.component.jsx"
 import ShopPage from "./pages/ShopPage/shop-page.component.jsx"
+import Layout from "./components/Layout/layout.component.jsx"
 
 // Here we define the routes and the corresponding components to render
 const App = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Homepage} />
-      <Route path="/shop" component={ShopPage} />
+    	  <Layout>
+		      <Route exact path="/" component={Homepage} />
+		      <Route exact path="/shop" component={ShopPage} />
+	      </Layout>
     </Switch>
   );
 }
---
+++ src/jsonData/navigation-links.js
@@ -0,0 +1,14 @@
+export default [
+	{
+		name: 'Shop',
+		link: 'shop'
+	},
+	{
+		name: 'Contact',
+		link: 'contact'
+	},
+	{
+		name: 'Signin',
+		link: 'signin'
+	}
+]
