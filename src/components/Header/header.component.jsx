d0203eb71e9f26897be80937eb4396c96b5add9a
Zakeria Ali <zak.fined@gmail.com>
2019-12-14 18:05:32 +0000

    Add headroom to the header

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 import {ReactComponent as Logo} from "../../images/crown.svg"
 import CartIcon from "../CartIcon/cart-icon.component.jsx"
 import CartDropdown from "../CartDropdown/cart-dropdown.component.jsx"
-
+import Headroom from "react-headroom"
 
 import {auth} from "../../firebase/firebase.utils"
 import {connect} from "react-redux"
@@ -11,27 +11,54 @@ import {connect} from "react-redux"
 import "./header.styles.scss"
 
 
-const Header = (props) => (
-	<nav className="header">
-		<Link to="/" className="header__logo">
-			<Logo />
-		</Link>
-		<ul className="header__list">
-			<li><Link to={`/shop`}>{`shop`.toUpperCase()}</Link></li>
-			<li><Link to={`/contact`}>{`contact`.toUpperCase()}</Link></li>
-			{props.currentUser ? 
-				(
-					<>
-						<li onClick={() => auth.signOut()}>{`signout`.toUpperCase()}</li>
-						<CartIcon />
-					</>
-				) : 
-				<li><Link to={`/signin`}>{`signin`.toUpperCase()}</Link></li>
-			}
-		</ul>
-		{props.openDropdown ? <CartDropdown /> : null}
-	</nav>
-)
+class Header extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.headerRef = React.createRef();
+	}
+
+	// componentDidMount(){
+	// 	const headroom = new Headroom(this.headerRef.current, {
+	// 	  "offset": 205,
+	// 	  "tolerance": 5,
+	// 	  "classes": {
+	// 	    "initial": "animated",
+	// 	    "pinned": "slideDown",
+	// 	    "unpinned": "slideUp"
+	// 		}
+	// 	});
+
+	// 	headroom.init();
+	// }
+
+	render(){
+		return (
+			<Headroom>
+				<nav className="header" ref={this.headerRef}>
+					<Link to="/" className="header__logo">
+						<Logo />
+					</Link>
+					<ul className="header__list">
+						<li><Link to={`/shop`}>{`shop`.toUpperCase()}</Link></li>
+						<li><Link to={`/contact`}>{`contact`.toUpperCase()}</Link></li>
+						{this.props.currentUser ? 
+							(
+								<>
+									<li onClick={() => auth.signOut()}>{`signout`.toUpperCase()}</li>
+									<CartIcon />
+								</>
+							) : 
+							<li><Link to={`/signin`}>{`signin`.toUpperCase()}</Link></li>
+						}
+					</ul>
+					{this.props.openDropdown ? <CartDropdown /> : null}
+				</nav>
+			</Headroom>
+		)
+	}
+}
+
 
 const mapStateToProps = state => ({
 	currentUser: state.user.currentUser,
